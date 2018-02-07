a8f47c57b811d8e7e0058158637d5e9601ae73db
Hobson, Chris (ch112h) <ch112h@us.att.com>
2018-02-07 12:01:17 -0800

    merging in version from npm for easier importing

--- README.md
+++ README.md
@@ -1,45 +1,19 @@
 # CSS-JSON Converter for JavaScript
 
-CSS-JSON Converter for JavaScript  
-Converts CSS to JSON and back.  
-Version 2.1  
+CSS-JSON Converter for JavaScript
+Converts CSS to JSON and back.
+
+Version 2.1.3
 
 Released under the MIT license.
 
-## Installation
-
-### Bower
-```bash
-bower install aramk-css-to-json --save
-```
-
-### NPM
-```bash
-npm install css-to-json
-```
-
 ## Usage
 
-### JavaScript
-```javascript
-// To JSON
-var json = CSSJSON.toJSON(cssString);
+	// To JSON
+	var json = CSSJSON.toJSON(cssString);
 
-// To CSS
-var css = CSSJSON.toCSS(jsonObject);
-
-// Appending to DOM (HTMLHeadElement)
-// alias (global): createCSS(css);
-// CSSJSON.toHEAD(json);
-CSSJSON.toHEAD(css); // returns HTMLStyleElement
-```
-
-### npm run / CLI
-To use CSSJSON from your command-line interface or with npm scripts there is [cssjson-cli](https://github.com/jakedex/cssjson-cli).
-
-```
-$ cssjson input_file [--to-css] [--output|-o output_file]
-```
+	// To CSS
+	var css = CSSJSON.toCSS(jsonObject);
 
 ## Sample
 
@@ -47,70 +21,66 @@ See example.html
 
 ### JSON
 
-```json
-{
-  "children": {
-    "@media (max-width: 800px)": {
+    {
       "children": {
-        "#main #comments": {
+        "@media (max-width: 800px)": {
+          "children": {
+            "#main #comments": {
+              "children": {},
+              "attributes": {
+                "margin": "0px",
+                "width": "auto",
+                "background": "red"
+              }
+            },
+            "#main #buttons": {
+              "children": {},
+              "attributes": {
+                "padding": "5px 10px",
+                "color": "blue"
+              }
+            }
+          },
+          "attributes": {}
+        },
+        "#main #content": {
           "children": {},
           "attributes": {
-            "margin": "0px",
-            "width": "auto",
-            "background": "red"
+            "margin": "0 7.6%",
+            "width": "auto"
           }
         },
-        "#main #buttons": {
+        "#nav-below": {
           "children": {},
           "attributes": {
-            "padding": "5px 10px",
-            "color": "blue"
+            "border-bottom": "1px solid #ddd",
+            "margin-bottom": "1.625em",
+            "background-image": "url(http://www.example.com/images/im.jpg)"
           }
         }
       },
       "attributes": {}
-    },
-    "#main #content": {
-      "children": {},
-      "attributes": {
-        "margin": "0 7.6%",
-        "width": "auto"
-      }
-    },
-    "#nav-below": {
-      "children": {},
-      "attributes": {
-        "border-bottom": "1px solid #ddd",
-        "margin-bottom": "1.625em",
-        "background-image": "url(http://www.example.com/images/im.jpg)"
-      }
     }
-  },
-  "attributes": {}
-}
-```
 
 ### CSS
 
-```css
-@media (max-width: 800px) {
-    #main #comments {
-        margin: 0px;
+    @media (max-width: 800px) {
+        #main #comments {
+            margin: 0px;
+            width: auto;
+            background: red;
+        }
+        #main #buttons {
+            padding: 5px 10px;
+            color: blue;
+        }
+    }
+    #main #content {
+        margin: 0 7.6%;
         width: auto;
-        background: red;
     }
-    #main #buttons {
-        padding: 5px 10px;
-        color: blue;
+    #nav-below {
+        border-bottom: 1px solid #ddd;
+        margin-bottom: 1.625em;
+        background-image: url(http://www.example.com/images/im.jpg);
     }
-}
-#main #content {
-    margin: 0 7.6%;
-    width: auto;
-}
-#nav-below {
-    border-bottom: 1px solid #ddd;
-    margin-bottom: 1.625em;
-    background-image: url(http://www.example.com/images/im.jpg);
-}
-```
--- cssjson.js
+++ cssjson.js
@@ -22,287 +22,236 @@
  THE SOFTWARE.
  */
 
-var CSSJSON = new function () {
 
-    var base = this;
+/*******************************************************************************
+ *  UMD pattern for exporting module
+ */
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.CSSJSON = factory();
+  }
+}(this, function () {
 
-    base.init = function () {
-        // String functions
-        String.prototype.trim = function () {
-            return this.replace(/^\s+|\s+$/g, '');
-        };
+    var CSSJSON = new function () {
 
-        String.prototype.repeat = function (n) {
-            return new Array(1 + n).join(this);
-        };
-    };
-    base.init();
+        var base = this;
 
-    var selX = /([^\s\;\{\}][^\;\{\}]*)\{/g;
-    var endX = /\}/g;
-    var lineX = /([^\;\{\}]*)\;/g;
-    var commentX = /\/\*[\s\S]*?\*\//g;
-    var lineAttrX = /([^\:]+):([^\;]*);/;
-
-    // This is used, a concatenation of all above. We use alternation to
-    // capture.
-    var altX = /(\/\*[\s\S]*?\*\/)|([^\s\;\{\}][^\;\{\}]*(?=\{))|(\})|([^\;\{\}]+\;(?!\s*\*\/))/gmi;
-
-    // Capture groups
-    var capComment = 1;
-    var capSelector = 2;
-    var capEnd = 3;
-    var capAttr = 4;
-
-    var isEmpty = function (x) {
-        return typeof x == 'undefined' || x.length == 0 || x == null;
-    };
-
-    var isCssJson = function (node) {
-        return !isEmpty(node) ? (node.attributes && node.children) : false;
-    }
-
-    /**
-     * Input is css string and current pos, returns JSON object
-     *
-     * @param cssString
-     *            The CSS string.
-     * @param args
-     *            An optional argument object. ordered: Whether order of
-     *            comments and other nodes should be kept in the output. This
-     *            will return an object where all the keys are numbers and the
-     *            values are objects containing "name" and "value" keys for each
-     *            node. comments: Whether to capture comments. split: Whether to
-     *            split each comma separated list of selectors.
-     */
-    base.toJSON = function (cssString, args) {
-        var node = {
-            children: {},
-            attributes: {}
-        };
-        var match = null;
-        var count = 0;
-
-        if (typeof args == 'undefined') {
-            var args = {
-                ordered: false,
-                comments: false,
-                stripComments: false,
-                split: false
+        base.init = function () {
+            // String functions
+            String.prototype.trim = function () {
+                return this.replace(/^\s+|\s+$/g, '');
             };
-        }
-        if (args.stripComments) {
-            args.comments = false;
-            cssString = cssString.replace(commentX, '');
-        }
 
-        while ((match = altX.exec(cssString)) != null) {
-            if (!isEmpty(match[capComment]) && args.comments) {
-                // Comment
-                var add = match[capComment].trim();
-                node[count++] = add;
-            } else if (!isEmpty(match[capSelector])) {
-                // New node, we recurse
-                var name = match[capSelector].trim();
-                // This will return when we encounter a closing brace
-                var newNode = base.toJSON(cssString, args);
-                if (args.ordered) {
-                    var obj = {};
-                    obj['name'] = name;
-                    obj['value'] = newNode;
-                    // Since we must use key as index to keep order and not
-                    // name, this will differentiate between a Rule Node and an
-                    // Attribute, since both contain a name and value pair.
-                    obj['type'] = 'rule';
-                    node[count++] = obj;
-                } else {
-                    if (args.split) {
-                        var bits = name.split(',');
-                    } else {
-                        var bits = [name];
-                    }
-                    for (var i = 0; i < bits.length; i++) {
-                        var sel = bits[i].trim();
-                        if (sel in node.children) {
-                            for (var att in newNode.attributes) {
-                                node.children[sel].attributes[att] = newNode.attributes[att];
-                            }
-                        } else {
-                            node.children[sel] = newNode;
-                        }
-                    }
-                }
-            } else if (!isEmpty(match[capEnd])) {
-                // Node has finished
-                return node;
-            } else if (!isEmpty(match[capAttr])) {
-                var line = match[capAttr].trim();
-                var attr = lineAttrX.exec(line);
-                if (attr) {
-                    // Attribute
-                    var name = attr[1].trim();
-                    var value = attr[2].trim();
+            String.prototype.repeat = function (n) {
+                return new Array(1 + n).join(this);
+            };
+        };
+        base.init();
+
+        var selX = /([^\s\;\{\}][^\;\{\}]*)\{/g;
+        var endX = /\}/g;
+        var lineX = /([^\;\{\}]*)\;/g;
+        var commentX = /\/\*[\s\S]*?\*\//g;
+        var lineAttrX = /([^\:]+):([^\;]*);/;
+
+        // This is used, a concatenation of all above. We use alternation to
+        // capture.
+        var altX = /(\/\*[\s\S]*?\*\/)|([^\s\;\{\}][^\;\{\}]*(?=\{))|(\})|([^\;\{\}]+\;(?!\s*\*\/))/gmi;
+
+        // Capture groups
+        var capComment = 1;
+        var capSelector = 2;
+        var capEnd = 3;
+        var capAttr = 4;
+
+        var isEmpty = function (x) {
+            return typeof x == 'undefined' || x.length == 0 || x == null;
+        };
+
+        /**
+         * Input is css string and current pos, returns JSON object
+         *
+         * @param cssString
+         *            The CSS string.
+         * @param args
+         *            An optional argument object. ordered: Whether order of
+         *            comments and other nodes should be kept in the output. This
+         *            will return an object where all the keys are numbers and the
+         *            values are objects containing "name" and "value" keys for each
+         *            node. comments: Whether to capture comments. split: Whether to
+         *            split each comma separated list of selectors.
+         */
+        base.toJSON = function (cssString, args) {
+            var node = {
+                children: {},
+                attributes: {}
+            };
+            var match = null;
+            var count = 0;
+
+            if (typeof args == 'undefined') {
+                var args = {
+                    ordered: false,
+                    comments: false,
+                    stripComments: false,
+                    split: false
+                };
+            }
+            if (args.stripComments) {
+                args.comments = false;
+                cssString = cssString.replace(commentX, '');
+            }
+
+            while ((match = altX.exec(cssString)) != null) {
+                if (!isEmpty(match[capComment]) && args.comments) {
+                    // Comment
+                    var add = match[capComment].trim();
+                    node[count++] = add;
+                } else if (!isEmpty(match[capSelector])) {
+                    // New node, we recurse
+                    var name = match[capSelector].trim();
+                    // This will return when we encounter a closing brace
+                    var newNode = base.toJSON(cssString, args);
                     if (args.ordered) {
                         var obj = {};
                         obj['name'] = name;
-                        obj['value'] = value;
-                        obj['type'] = 'attr';
+                        obj['value'] = newNode;
+                        // Since we must use key as index to keep order and not
+                        // name, this will differentiate between a Rule Node and an
+                        // Attribute, since both contain a name and value pair.
+                        obj['type'] = 'rule';
                         node[count++] = obj;
                     } else {
-                        if (name in node.attributes) {
-                            var currVal = node.attributes[name];
-                            if (!(currVal instanceof Array)) {
-                                node.attributes[name] = [currVal];
-                            }
-                            node.attributes[name].push(value);
+                        if (args.split) {
+                            var bits = name.split(',');
                         } else {
-                            node.attributes[name] = value;
+                            var bits = [name];
+                        }
+                        for (i in bits) {
+                            var sel = bits[i].trim();
+                            if (sel in node.children) {
+                                for (var att in newNode.attributes) {
+                                    node.children[sel].attributes[att] = newNode.attributes[att];
+                                }
+                            } else {
+                                node.children[sel] = newNode;
+                            }
                         }
                     }
-                } else {
-                    // Semicolon terminated line
-                    node[count++] = line;
-                }
-            }
-        }
-
-        return node;
-    };
-
-    /**
-     * @param node
-     *            A JSON node.
-     * @param depth
-     *            The depth of the current node; used for indentation and
-     *            optional.
-     * @param breaks
-     *            Whether to add line breaks in the output.
-     */
-    base.toCSS = function (node, depth, breaks) {
-        var cssString = '';
-        if (typeof depth == 'undefined') {
-            depth = 0;
-        }
-        if (typeof breaks == 'undefined') {
-            breaks = false;
-        }
-        if (node.attributes) {
-            for (i in node.attributes) {
-                var att = node.attributes[i];
-                if (att instanceof Array) {
-                    for (var j = 0; j < att.length; j++) {
-                        cssString += strAttr(i, att[j], depth);
-                    }
-                } else {
-                    cssString += strAttr(i, att, depth);
-                }
-            }
-        }
-        if (node.children) {
-            var first = true;
-            var process = (name,data)=>{
-                if (breaks && !first) {
-                    cssString += '\n';
-                } else {
-                    first = false;
-                }
-                cssString += strNode(name, data, depth);
-            }
-            if (node.children instanceof Array) {
-                for(var i=0; i<node.children.length; i++){
-                    for (var j in node.children[i]) {
-                        process(j,node.children[i][j])
+                } else if (!isEmpty(match[capEnd])) {
+                    // Node has finished
+                    return node;
+                } else if (!isEmpty(match[capAttr])) {
+                    var line = match[capAttr].trim();
+                    var attr = lineAttrX.exec(line);
+                    if (attr) {
+                        // Attribute
+                        var name = attr[1].trim();
+                        var value = attr[2].trim();
+                        if (args.ordered) {
+                            var obj = {};
+                            obj['name'] = name;
+                            obj['value'] = value;
+                            obj['type'] = 'attr';
+                            node[count++] = obj;
+                        } else {
+                            if (name in node.attributes) {
+                                var currVal = node.attributes[name];
+                                if (!(currVal instanceof Array)) {
+                                    node.attributes[name] = [currVal];
+                                }
+                                node.attributes[name].push(value);
+                            } else {
+                                node.attributes[name] = value;
+                            }
+                        }
+                    } else {
+                        // Semicolon terminated line
+                        node[count++] = line;
                     }
                 }
-            }else{
-                for (var i in node.children) {
-                    process(i,node.children[i])
+            }
+
+            return node;
+        };
+
+        /**
+         * @param node
+         *            A JSON node.
+         * @param depth
+         *            The depth of the current node; used for indentation and
+         *            optional.
+         * @param breaks
+         *            Whether to add line breaks in the output.
+         */
+        base.toCSS = function (node, depth, breaks) {
+            var cssString = '';
+            if (typeof depth == 'undefined') {
+                depth = 0;
+            }
+            if (typeof breaks == 'undefined') {
+                breaks = false;
+            }
+            if (node.attributes) {
+                for (i in node.attributes) {
+                    var att = node.attributes[i];
+                    if (att instanceof Array) {
+                        for (var j = 0; j < att.length; j++) {
+                            cssString += strAttr(i, att[j], depth);
+                        }
+                    } else {
+                        cssString += strAttr(i, att, depth);
+                    }
                 }
             }
-        }
-        return cssString;
-    };
-
-    /**
-     * @param data
-     *            You can pass css string or the CSSJS.toJSON return value.
-     * @param id (Optional)
-     *            To identify and easy removable of the style element
-     * @param replace (Optional. defaults to TRUE)
-     *            Whether to remove or simply do nothing
-     * @return HTMLLinkElement
-     */
-    base.toHEAD = function (data, id, replace) {
-        var head = document.getElementsByTagName('head')[0];
-        var xnode = document.getElementById(id);
-        var _xnodeTest = (xnode !== null && xnode instanceof HTMLStyleElement);
-
-        if (isEmpty(data) || !(head instanceof HTMLHeadElement)) return;
-        if (_xnodeTest) {
-            if (replace === true || isEmpty(replace)) {
-                xnode.removeAttribute('id');
-            } else return;
-        }
-        if (isCssJson(data)) {
-            data = base.toCSS(data);
-        }
-
-        var node = document.createElement('style');
-        node.type = 'text/css';
-
-        if (!isEmpty(id)) {
-            node.id = id;
-        } else {
-            node.id = 'cssjson_' + timestamp();
-        }
-        if (node.styleSheet) {
-            node.styleSheet.cssText = data;
-        } else {
-            node.appendChild(document.createTextNode(data));
-        }
-
-        head.appendChild(node);
-
-        if (isValidStyleNode(node)) {
-            if (_xnodeTest) {
-                xnode.parentNode.removeChild(xnode);
+            if (node.children) {
+                var first = true;
+                var process = (name,data)=>{
+                    if (breaks && !first) {
+                        cssString += '\n';
+                    } else {
+                        first = false;
+                    }
+                    cssString += strNode(name, data, depth);
+                }
+                if (node.children instanceof Array) {
+                    for(var i=0; i<node.children.length; i++){
+                        for (var j in node.children[i]) {
+                            process(j,node.children[i][j])
+                        }
+                    }
+                }else{
+                    for (var i in node.children) {
+                        process(i,node.children[i])
+                    }
+                }
             }
-        } else {
-            node.parentNode.removeChild(node);
-            if (_xnodeTest) {
-                xnode.setAttribute('id', id);
-                node = xnode;
-            } else return;
-        }
+            return cssString;
+        };
 
-        return node;
+        // Helpers
+
+        var strAttr = function (name, value, depth) {
+            return '\t'.repeat(depth) + name + ': ' + value + ';\n';
+        };
+
+        var strNode = function (name, value, depth) {
+            var cssString = '\t'.repeat(depth) + name + ' {\n';
+            cssString += base.toCSS(value, depth + 1);
+            cssString += '\t'.repeat(depth) + '}\n';
+            return cssString;
+        };
+
     };
 
-    // Alias
-
-    if (typeof window != 'undefined') {
-        window.createCSS = base.toHEAD;
-    }
-
-    // Helpers
-
-    var isValidStyleNode = function (node) {
-        return (node instanceof HTMLStyleElement) && node.sheet.cssRules.length > 0;
-    }
-
-    var timestamp = function () {
-        return Date.now() || +new Date();
-    };
-
-    var strAttr = function (name, value, depth) {
-        return '\t'.repeat(depth) + name + ': ' + value + ';\n';
-    };
-
-    var strNode = function (name, value, depth) {
-        var cssString = '\t'.repeat(depth) + name + ' {\n';
-        cssString += base.toCSS(value, depth + 1);
-        cssString += '\t'.repeat(depth) + '}\n';
-        return cssString;
-    };
-
-};
+    return CSSJSON;
+}));
